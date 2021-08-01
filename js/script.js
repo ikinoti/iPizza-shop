add1f2eff1b93be0fc83fd0e62c5a4ea27e380b0
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-01 22:36:17 +0300

    create prototype for pizza object to determine the cost and take topping choosed

--- js/script.js
+++ js/script.js
@@ -6,6 +6,37 @@ function Pizza(pizzaSize, pizzaCrust) {
   this.price = 0;
 }
 
+Pizza.prototype.cost = function () {
+  var price = 0;
+  if (this.pizzaSize == "large") {
+    price += 1200;
+  } else if (this.pizzaSize == "Medium") {
+    price += 800;
+  } else {
+    price += 650;
+  }
+
+  for (var i = 0; i < this.toppings.length; i++) {
+    price += 100;
+  }
+  if (this.crust == "cheese") {
+    price += 300;
+  } else if (this.crust == "sicilian") {
+    price += 250;
+  } else {
+    price += 200;
+  }
+  this.price = price;
+};
+
+Pizza.prototype.toppingList = function () {
+  if (this.toppings.length > 0) {
+    return this.toppings.join(", ");
+  } else {
+    return "None";
+  }
+};
+
 // USER INTERFACE LOGIC
 $(document).ready(function () {
   var total = 0;
